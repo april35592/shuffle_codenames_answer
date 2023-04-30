c9bdabfc510580b80f348fbaa7b5636f40c00ea4
사월 <77724156+april35592@users.noreply.github.com>
2023-04-30 18:26:55 +0900

    encapsulation

--- boardgame-codenames/script.js
+++ boardgame-codenames/script.js
@@ -3,39 +3,48 @@ const tableList = document.getElementsByClassName("table");
 const container = document.getElementById("container");
 const header = document.getElementById("header");
 
-const spy = -1;
-const redAgent = 1;
-const blueAgent = 2;
-const citizen = 0;
+const codenames = {
+  size: tableList.length,
+  team: {
+    spy: -1,
+    redAgent: 1,
+    blueAgent: 2,
+    citizen: 0,
+    doubleAgent: {
+      value: 0,
+      shuffle() {
+        codenames.team.doubleAgent.value = randomOneTwo();
+      },
+    },
+  },
+  array: {
+    value: [],
+    setting: () => {
+      codenames.array.value = [
+        ...pushArr(codenames.team.spy, 1),
+        ...pushArr(codenames.team.doubleAgent.value, 1),
+        ...pushArr(codenames.team.redAgent, 7),
+        ...pushArr(codenames.team.blueAgent, 7),
+        ...pushArr(codenames.team.citizen, codenames.size - 16),
+      ];
+    },
+    shuffle: () => {
+      codenames.array.setting();
+      codenames.array.value = arrayShuffle(codenames.array.value);
+      return codenames.array.value;
+    },
+  },
+  shuffle: () => {
+    codenames.team.doubleAgent.shuffle();
+    return codenames.array.shuffle();
+  },
+};
 
-shuffleBtn.addEventListener("click", shuffle);
+shuffleBtn.addEventListener("click", clickShuffle);
 
-function shuffle() {
-  const array = arrayShuffle(codenamesArray(tableList.length));
-  for (let i = 0; i < tableList.length; i++) {
-    tableList.item(i).classList.remove("spy", "red", "blue");
-    if (array[i] === spy) {
-      tableList.item(i).classList.add("spy");
-    } else if (array[i] === redAgent) {
-      tableList.item(i).classList.add("red");
-    } else if (array[i] === blueAgent) {
-      tableList.item(i).classList.add("blue");
-    }
-  }
-}
-
-function codenamesArray(length) {
-  const doubleAgent = randomRadBlue();
-
-  const array = [
-    ...pushArr(spy, 1),
-    ...pushArr(doubleAgent, 1),
-    ...pushArr(redAgent, 7),
-    ...pushArr(blueAgent, 7),
-    ...pushArr(citizen, length - 16),
-  ];
-
-  return array;
+function clickShuffle() {
+  paintTable(codenames.shuffle());
+  sirenFirstTurn(codenames.team.doubleAgent.value);
 }
 
 function pushArr(agent, number) {
@@ -46,14 +55,22 @@ function pushArr(agent, number) {
   return array;
 }
 
-function randomRadBlue() {
-  const doubleAgent = Math.floor(Math.random() * 2) + 1;
-  siren(doubleAgent);
-  return doubleAgent;
+function randomOneTwo() {
+  return Math.floor(Math.random() * 2) + 1;
 }
 
-function siren(doubleAgent) {
-  if (doubleAgent === redAgent) {
+function arrayShuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomPosition = Math.floor(Math.random() * (i + 1));
+    const temporary = array[i];
+    array[i] = array[randomPosition];
+    array[randomPosition] = temporary;
+  }
+  return array;
+}
+
+function sirenFirstTurn(whoIs) {
+  if (whoIs === codenames.team.redAgent) {
     container.classList.remove("blue");
     container.classList.add("red");
     header.classList.remove("blue");
@@ -66,12 +83,15 @@ function siren(doubleAgent) {
   }
 }
 
-function arrayShuffle(array) {
-  for (let index = array.length - 1; index > 0; index--) {
-    const randomPosition = Math.floor(Math.random() * (index + 1));
-    const temporary = array[index];
-    array[index] = array[randomPosition];
-    array[randomPosition] = temporary;
+function paintTable(array) {
+  for (let i = 0; i < tableList.length; i++) {
+    tableList.item(i).classList.remove("spy", "red", "blue");
+    if (array[i] === codenames.team.spy) {
+      tableList.item(i).classList.add("spy");
+    } else if (array[i] === codenames.team.redAgent) {
+      tableList.item(i).classList.add("red");
+    } else if (array[i] === codenames.team.blueAgent) {
+      tableList.item(i).classList.add("blue");
+    }
   }
-  return array;
 }
